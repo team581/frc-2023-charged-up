652e831c30462cfc9ce71f0ad0c11a98fe6accbb
Jonah Snider <jonah@jonahsnider.com>
2023-01-09 23:47:00 -0800

    Move LifecycleSubsystem to util directory

--- src/main/java/frc/robot/util/LifecycleSubsystem.java
+++ src/main/java/frc/robot/util/LifecycleSubsystem.java
@@ -2,11 +2,12 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+package frc.robot.util;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.IterativeRobotBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
 
 /**
  * Extends {@link SubsystemBase} by adding in lifecycle methods for robotInit, teleopPeriodic, etc.,
