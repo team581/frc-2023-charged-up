71a200e2aa0701b8ddd2dfc7480358f1918b0f10
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2023-01-14 14:00:37 -0800

    Added homing sequence to elevator

--- src/main/java/frc/robot/elevator/ElevatorSubsystem.java
+++ src/main/java/frc/robot/elevator/ElevatorSubsystem.java
@@ -15,12 +15,17 @@ public class ElevatorSubsystem extends LifecycleSubsystem {
   private final TalonFX motor;
   private double goalPositionInInches = 0;
   private double sensorUnitsPerElevatorInch = 0;
+  private boolean isHoming = false;
 
   public ElevatorSubsystem(TalonFX motor) {
     this.motor = motor;
 
     // Set pid for slot 0
     // Set motion magic
+  }
+
+  public void startHoming() {
+    this.isHoming = true;
   }
 
   public double getPosition() {
@@ -38,12 +43,23 @@ public class ElevatorSubsystem extends LifecycleSubsystem {
    @Override
   public void robotPeriodic() {
     Logger.getInstance().recordOutput("Elevator/Position", getPosition());
+    Logger.getInstance().recordOutput("Elevator/Current", motor.getSupplyCurrent());
   }
 
   @Override
   public void enabledPeriodic() {
+    // Add homing sequence
     // Convert goal in inches to sensor units, and set motor
+
+    if (isHoming) {
+      motor.set(ControlMode.PercentOutput, -0.0001);
+      double current = motor.getSupplyCurrent();
+      if (current > 0.015)
+      motor.setSelectedSensorPosition(0);
+      this.isHoming = false;
+    } else {
     double goalPositionInSensorUnits = goalPositionInInches * sensorUnitsPerElevatorInch;
     motor.set(ControlMode.Position, goalPositionInSensorUnits);
+    }
   }
 }
