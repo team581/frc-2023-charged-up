96cb642491c16522fb26e6869a069cef0156c682
thebrossj2 <87281874+simonp17@users.noreply.github.com>
2023-01-19 18:20:10 -0800

    convert to pose2d

--- src/main/java/frc/robot/localization/LocalizationSubsystem.java
+++ src/main/java/frc/robot/localization/LocalizationSubsystem.java
@@ -8,22 +8,31 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import org.littletonrobotics.junction.Logger;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.util.LifecycleSubsystem;
 
-public class LocalizationSubsystem extends SubsystemBase {
-  double[] botpose = {0,0,0,0,0,0};
+public class LocalizationSubsystem extends LifecycleSubsystem {
+  private Pose2d botPose = new Pose2d();
   
   public LocalizationSubsystem() {
   
   }
 
   @Override
-  public void periodic() {
-    double[] zeroArray ={0,0,0,0,0,0};
-    botpose = NetworkTableInstance.getDefault().getTable("limelight").getEntry("botpose").getDoubleArray(zeroArray);
-    Logger.getInstance().recordOutput("Bot Position", botpose);
+  public void robotPeriodic() {
+    double[] emptyArray ={};
+    double[] rawPose = NetworkTableInstance.getDefault().getTable("limelight").getEntry("botpose").getDoubleArray(emptyArray);
+    
+    if (rawPose.length > 0) {
+      botPose =  new Pose2d(rawPose[0], rawPose[1], Rotation2d.fromDegrees(rawPose[4]));
+      Logger.getInstance().recordOutput("Bot Position", botPose);
+    }
   }
 }
