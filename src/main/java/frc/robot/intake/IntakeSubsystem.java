51f7a7b56b2f87ffc15549ed6a92d1a4c12a3876
Owen <117956892+Owen756@users.noreply.github.com>
2023-02-04 13:53:07 -0800

    start tuning intake

--- src/main/java/frc/robot/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/intake/IntakeSubsystem.java
@@ -7,6 +7,7 @@ package frc.robot.intake;
 import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import edu.wpi.first.math.filter.LinearFilter;
 import frc.robot.util.LifecycleSubsystem;
 import org.littletonrobotics.junction.Logger;
 
@@ -21,6 +22,8 @@ public class IntakeSubsystem extends LifecycleSubsystem {
 
   private final TalonFX motor;
 
+  private final LinearFilter filter = LinearFilter.movingAverage(5);
+
   public IntakeSubsystem(TalonFX motor) {
     this.motor = motor;
     motor.setInverted(true);
@@ -32,30 +35,32 @@ public class IntakeSubsystem extends LifecycleSubsystem {
     Logger.getInstance().recordOutput("Intake/Mode", mode.toString());
     Logger.getInstance().recordOutput("Intake/HeldGamePiece", gamePiece.toString());
     Logger.getInstance().recordOutput("Intake/Current", motor.getStatorCurrent());
+    Logger.getInstance().recordOutput("Intake/Voltage", motor.getMotorOutputVoltage());
   }
 
   @Override
   public void enabledPeriodic() {
-    // TODO: Tune voltages and currents
+    double current = filter.calculate(motor.getStatorCurrent());
+    Logger.getInstance().recordOutput("Intake/FilteredCurrent", current);
     if (mode == IntakeMode.INTAKE_CUBE) {
-      motor.set(TalonFXControlMode.PercentOutput, 0.2);
-      if (motor.getStatorCurrent() > 10) {
+      motor.set(TalonFXControlMode.PercentOutput, 0.4);
+      if (current > 40) {
         gamePiece = HeldGamePiece.CUBE;
       }
     } else if (mode == IntakeMode.INTAKE_CONE) {
       motor.set(TalonFXControlMode.PercentOutput, -0.2);
-      if (motor.getStatorCurrent() > 20) {
+      if (current > 999) {
         gamePiece = HeldGamePiece.CONE;
       }
     } else if (mode == IntakeMode.OUTTAKE) {
       if (gamePiece == HeldGamePiece.CUBE) {
-        motor.set(TalonFXControlMode.PercentOutput, -0.2);
-        if (motor.getStatorCurrent() < 5) {
+        motor.set(TalonFXControlMode.PercentOutput, -0.3);
+        if (current < 13 && current > 5) {
           gamePiece = HeldGamePiece.NOTHING;
         }
       } else if (gamePiece == HeldGamePiece.CONE) {
         motor.set(TalonFXControlMode.PercentOutput, 0.2);
-        if (motor.getStatorCurrent() < 5) {
+        if (current < -999) {
           gamePiece = HeldGamePiece.NOTHING;
         }
       }
