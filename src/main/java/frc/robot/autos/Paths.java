200e516eea52230160d57e103c07c1d290e77afc
rkanemoto <73001957+rkanemoto@users.noreply.github.com>
2023-02-04 16:56:16 -0800

    Create auto path

--- src/main/java/frc/robot/autos/Paths.java
+++ src/main/java/frc/robot/autos/Paths.java
@@ -10,6 +10,7 @@ import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.PathPoint;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import java.util.ArrayList;
 
 public class Paths {
   private Paths() {}
@@ -28,4 +29,16 @@ public class Paths {
               Rotation2d.fromDegrees(
                   -90) // position, heading(direction of travel), holonomic rotation
               ));
+  private static ArrayList<PathPoint> driveForwardList =
+      new ArrayList<PathPoint>() {
+        {
+          add(
+              new PathPoint(
+                  new Translation2d(0.0, 4.0),
+                  Rotation2d.fromDegrees(0),
+                  Rotation2d.fromDegrees(0)));
+        }
+      };
+  public static final PathPlannerTrajectory DRIVE_FORWARD =
+      PathPlanner.generatePath(new PathConstraints(4, 3), driveForwardList);
 }
--- src/main/java/frc/robot/managers/SuperstructureManager.java
+++ src/main/java/frc/robot/managers/SuperstructureManager.java
@@ -68,20 +68,20 @@ public class SuperstructureManager extends LifecycleSubsystem {
   public Command getManualScoreCommand(ScoringLocation scoringLocation) {
     if (scoringLocation == ScoringLocation.LOW) {
       return Commands.either(
-              getCommand(States.CUBE_NODE_LOW),
-              getCommand(States.CONE_NODE_LOW),
-              () -> intake.getGamePiece() == HeldGamePiece.CUBE);
+          getCommand(States.CUBE_NODE_LOW),
+          getCommand(States.CONE_NODE_LOW),
+          () -> intake.getGamePiece() == HeldGamePiece.CUBE);
     } else if (scoringLocation == ScoringLocation.MID) {
       return Commands.either(
-              getCommand(States.CUBE_NODE_MID),
-              getCommand(States.CONE_NODE_MID),
-              () -> intake.getGamePiece() == HeldGamePiece.CUBE);
+          getCommand(States.CUBE_NODE_MID),
+          getCommand(States.CONE_NODE_MID),
+          () -> intake.getGamePiece() == HeldGamePiece.CUBE);
 
     } else {
       return Commands.either(
-              getCommand(States.CUBE_NODE_HIGH),
-              getCommand(States.CONE_NODE_HIGH),
-              () -> intake.getGamePiece() == HeldGamePiece.CUBE);
+          getCommand(States.CUBE_NODE_HIGH),
+          getCommand(States.CONE_NODE_HIGH),
+          () -> intake.getGamePiece() == HeldGamePiece.CUBE);
     }
   }
 
