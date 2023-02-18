3a62ede23709a344b611df3441e7ee973ecc2295
thebrossj2 <87281874+simonp17@users.noreply.github.com>
2023-02-18 11:52:14 -0800

    update code for testing

--- src/main/java/frc/robot/swerve/SwerveSubsystem.java
+++ src/main/java/frc/robot/swerve/SwerveSubsystem.java
@@ -41,6 +41,8 @@ public class SwerveSubsystem extends LifecycleSubsystem {
   private final SwerveModule frontLeft;
   private final SwerveModule backRight;
   private final SwerveModule backLeft;
+  private boolean doneResetting = false;
+
 
   public SwerveSubsystem(
       ImuSubsystem imu,
@@ -57,7 +59,6 @@ public class SwerveSubsystem extends LifecycleSubsystem {
 
   @Override
   public void disabledPeriodic() {
-    // TODO: This causes constants loop overruns when it runs during disabled
     frontRight.resetWheelAngle();
     frontLeft.resetWheelAngle();
     backRight.resetWheelAngle();
