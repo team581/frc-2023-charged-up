73f714bd636d1c822221164530313da3e4f95bbc
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2023-02-26 13:37:51 -0800

    Restore Tyke bumper measurement

--- src/main/java/frc/robot/config/Config.java
+++ src/main/java/frc/robot/config/Config.java
@@ -142,7 +142,7 @@ public class Config {
       IS_SPIKE ? Rotation2d.fromDegrees(50) : Rotation2d.fromDegrees(13);
 
   public static final double ROBOT_CENTER_TO_FRONT =
-      IS_SPIKE ? Units.inchesToMeters(20) : Units.inchesToMeters(50);
+      IS_SPIKE ? Units.inchesToMeters(20) : Units.inchesToMeters(17.5);
 
   private Config() {}
 }
