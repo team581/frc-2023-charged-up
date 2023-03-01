be6c952826cfc666254bd96dfc4c643bd97fad26
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2023-02-28 17:09:32 -0800

    Add FMS subsystem

---
+++ src/main/java/frc/robot/fms/FmsSubsystem.java
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.fms;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import frc.robot.util.scheduling.LifecycleSubsystem;
+import frc.robot.util.scheduling.SubsystemPriority;
+import org.littletonrobotics.junction.Logger;
+
+public class FmsSubsystem extends LifecycleSubsystem {
+
+  public FmsSubsystem() {
+    super(SubsystemPriority.FMS);
+  }
+
+  public static boolean isRedAlliance() {
+    if (DriverStation.getAlliance() == Alliance.Red
+        || DriverStation.getAlliance() == Alliance.Invalid) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  @Override
+  public void robotPeriodic() {
+    Logger.getInstance().recordOutput("Fms/Alliance", isRedAlliance() ? "Red" : "Blue");
+  }
+}
