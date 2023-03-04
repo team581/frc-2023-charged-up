f466f6dc3804e5cec0012562b1b3f932dc270a74
Jonah Snider <jonah@jonahsnider.com>
2023-03-03 20:28:45 -0800

    Fix auto paths with stop points

--- src/main/java/frc/robot/autos/Paths.java
+++ src/main/java/frc/robot/autos/Paths.java
@@ -7,40 +7,41 @@ package frc.robot.autos;
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
+import java.util.List;
 
 public class Paths {
   private Paths() {}
 
-  public static final PathPlannerTrajectory BLUE_LONG_SIDE_1_CONE =
-      PathPlanner.loadPath("BlueLongSide1Cone", new PathConstraints(1.0, 0.75));
-  public static final PathPlannerTrajectory BLUE_SHORT_SIDE_1_CONE =
-      PathPlanner.loadPath("BlueShortSide1Cone", new PathConstraints(1, 0.75));
-  public static final PathPlannerTrajectory BLUE_LONG_SIDE_1_5_CONE_BALANCE =
-      PathPlanner.loadPath("BlueLongSide1.5ConeBalance", new PathConstraints(1.5, 0.75));
-  public static final PathPlannerTrajectory BLUE_MID_1_5_CONE_BALANCE =
-      PathPlanner.loadPath("BlueMid1.5ConeBalance", new PathConstraints(2.5, 0.75));
-  public static final PathPlannerTrajectory BLUE_MID_1_CONE_BALANCE =
-      PathPlanner.loadPath("BlueMid1ConeBalance", new PathConstraints(1.5, 2.5));
-  public static final PathPlannerTrajectory BLUE_SHORT_SIDE_2_5_CONE_BALANCE =
-      PathPlanner.loadPath("BlueShortSide2.5ConeBalance", new PathConstraints(4.0, 3.0));
-  public static final PathPlannerTrajectory BLUE_SHORT_SIDE_2_CONE_BALANCE =
-      PathPlanner.loadPath("BlueShortSide2ConeBalance", new PathConstraints(1.75, 2.0));
+  public static final List<PathPlannerTrajectory> BLUE_LONG_SIDE_1_CONE =
+      PathPlanner.loadPathGroup("BlueLongSide1Cone", new PathConstraints(1.0, 0.75));
+  public static final List<PathPlannerTrajectory> BLUE_SHORT_SIDE_1_CONE =
+      PathPlanner.loadPathGroup("BlueShortSide1Cone", new PathConstraints(1, 0.75));
+  public static final List<PathPlannerTrajectory> BLUE_LONG_SIDE_1_5_CONE_BALANCE =
+      PathPlanner.loadPathGroup("BlueLongSide1.5ConeBalance", new PathConstraints(1.5, 0.75));
+  public static final List<PathPlannerTrajectory> BLUE_MID_1_5_CONE_BALANCE =
+      PathPlanner.loadPathGroup("BlueMid1.5ConeBalance", new PathConstraints(2.5, 0.75));
+  public static final List<PathPlannerTrajectory> BLUE_MID_1_CONE_BALANCE =
+      PathPlanner.loadPathGroup("BlueMid1ConeBalance", new PathConstraints(1.5, 2.5));
+  public static final List<PathPlannerTrajectory> BLUE_SHORT_SIDE_2_5_CONE_BALANCE =
+      PathPlanner.loadPathGroup("BlueShortSide2.5ConeBalance", new PathConstraints(4.0, 3.0));
+  public static final List<PathPlannerTrajectory> BLUE_SHORT_SIDE_2_CONE_BALANCE =
+      PathPlanner.loadPathGroup("BlueShortSide2ConeBalance", new PathConstraints(1.75, 2.0));
 
-  public static final PathPlannerTrajectory RED_LONG_SIDE_1_CONE =
-      PathPlanner.loadPath("RedLongSide1Cone", new PathConstraints(1, 0.75));
-  public static final PathPlannerTrajectory RED_SHORT_SIDE_1_CONE =
-      PathPlanner.loadPath("RedShortSide1Cone", new PathConstraints(1.0, 0.75));
-  public static final PathPlannerTrajectory RED_LONG_SIDE_1_5_CONE_BALANCE =
-      PathPlanner.loadPath("RedLongSide1.5ConeBalance", new PathConstraints(1.5, 0.75));
-  public static final PathPlannerTrajectory RED_MID_1_5_CONE_BALANCE =
-      PathPlanner.loadPath("RedMid1.5ConeBalance", new PathConstraints(4, 3));
-  public static final PathPlannerTrajectory RED_MID_1_CONE_BALANCE =
-      PathPlanner.loadPath("RedMid1ConeBalance", new PathConstraints(1.5, 2.5));
-  public static final PathPlannerTrajectory RED_SHORT_SIDE_2_5_CONE_BALANCE =
-      PathPlanner.loadPath("RedShortSide2.5ConeBalance", new PathConstraints(4.0, 3.0));
-  public static final PathPlannerTrajectory RED_SHORT_SIDE_2_CONE_BALANCE =
-      PathPlanner.loadPath("RedShortSide2ConeBalance", new PathConstraints(1, 1));
+  public static final List<PathPlannerTrajectory> RED_LONG_SIDE_1_CONE =
+      PathPlanner.loadPathGroup("RedLongSide1Cone", new PathConstraints(1, 0.75));
+  public static final List<PathPlannerTrajectory> RED_SHORT_SIDE_1_CONE =
+      PathPlanner.loadPathGroup("RedShortSide1Cone", new PathConstraints(1.0, 0.75));
+  public static final List<PathPlannerTrajectory> RED_LONG_SIDE_1_5_CONE_BALANCE =
+      PathPlanner.loadPathGroup("RedLongSide1.5ConeBalance", new PathConstraints(1.5, 0.75));
+  public static final List<PathPlannerTrajectory> RED_MID_1_5_CONE_BALANCE =
+      PathPlanner.loadPathGroup("RedMid1.5ConeBalance", new PathConstraints(4, 3));
+  public static final List<PathPlannerTrajectory> RED_MID_1_CONE_BALANCE =
+      PathPlanner.loadPathGroup("RedMid1ConeBalance", new PathConstraints(1.5, 2.5));
+  public static final List<PathPlannerTrajectory> RED_SHORT_SIDE_2_5_CONE_BALANCE =
+      PathPlanner.loadPathGroup("RedShortSide2.5ConeBalance", new PathConstraints(4.0, 3.0));
+  public static final List<PathPlannerTrajectory> RED_SHORT_SIDE_2_CONE_BALANCE =
+      PathPlanner.loadPathGroup("RedShortSide2ConeBalance", new PathConstraints(1, 1));
 
-  public static final PathPlannerTrajectory BUG =
-      PathPlanner.loadPath("bug", new PathConstraints(1, 1));
+  public static final List<PathPlannerTrajectory> BUG =
+      PathPlanner.loadPathGroup("bug", new PathConstraints(1, 1));
 }
