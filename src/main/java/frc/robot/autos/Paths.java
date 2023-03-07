14ce96c271734e3314570007d7d58128f23ea9dd
Jonah Snider <jonah@jonahsnider.com>
2023-03-07 14:28:29 -0800

    Rewrite autos and paths to be more memory efficient

--- src/main/java/frc/robot/autos/Paths.java
+++ src/main/java/frc/robot/autos/Paths.java
@@ -7,40 +7,50 @@ package frc.robot.autos;
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Paths {
+  private static final boolean CACHING_ENABLED = true;
+
+  private static Paths instance;
+
+  public static Paths getInstance() {
+    if (instance == null) {
+      instance = new Paths();
+    }
+
+    return instance;
+  }
+
+  private final Map<String, WeakReference<List<PathPlannerTrajectory>>> cache = new HashMap<>();
+
   private Paths() {}
 
-  public static final List<PathPlannerTrajectory> BLUE_LONG_SIDE_1_CONE =
-      PathPlanner.loadPathGroup("BlueLongSide1", new PathConstraints(1.0, 0.75));
-  public static final List<PathPlannerTrajectory> BLUE_SHORT_SIDE_1_CONE =
-      PathPlanner.loadPathGroup("BlueShortSide1", new PathConstraints(1, 0.75));
-  public static final List<PathPlannerTrajectory> BLUE_LONG_SIDE_1_5_CONE_BALANCE =
-      PathPlanner.loadPathGroup("BlueLongSide1.5Balance", new PathConstraints(1.5, 0.75));
-  public static final List<PathPlannerTrajectory> BLUE_MID_1_5_CONE_BALANCE =
-      PathPlanner.loadPathGroup("BlueMid1.5Balance", new PathConstraints(2.5, 0.75));
-  public static final List<PathPlannerTrajectory> BLUE_MID_1_CONE_BALANCE =
-      PathPlanner.loadPathGroup("BlueMid1Balance", new PathConstraints(1.5, 2.5));
-  public static final List<PathPlannerTrajectory> BLUE_SHORT_SIDE_2_5_CONE_BALANCE =
-      PathPlanner.loadPathGroup("BlueShortSide2.5Balance", new PathConstraints(4.0, 3.0));
-  public static final List<PathPlannerTrajectory> BLUE_SHORT_SIDE_2_CONE_BALANCE =
-      PathPlanner.loadPathGroup("BlueShortSide2Balance", new PathConstraints(1.75, 2.0));
+  public List<PathPlannerTrajectory> getPath(AutoKind auto) {
+    if (auto == AutoKind.DO_NOTHING) {
+      return List.of();
+    }
 
-  public static final List<PathPlannerTrajectory> RED_LONG_SIDE_1_CONE =
-      PathPlanner.loadPathGroup("RedLongSide1", new PathConstraints(1, 0.75));
-  public static final List<PathPlannerTrajectory> RED_SHORT_SIDE_1_CONE =
-      PathPlanner.loadPathGroup("RedShortSide1", new PathConstraints(1.0, 0.75));
-  public static final List<PathPlannerTrajectory> RED_LONG_SIDE_1_5_CONE_BALANCE =
-      PathPlanner.loadPathGroup("RedLongSide1.5Balance", new PathConstraints(1.5, 0.75));
-  public static final List<PathPlannerTrajectory> RED_MID_1_5_CONE_BALANCE =
-      PathPlanner.loadPathGroup("RedMid1.5Balance", new PathConstraints(3.3, 2.0));
-  public static final List<PathPlannerTrajectory> RED_MID_1_CONE_BALANCE =
-      PathPlanner.loadPathGroup("RedMid1Balance", new PathConstraints(1.5, 2.5));
-  public static final List<PathPlannerTrajectory> RED_LONG_SIDE_2_5_CONE_BALANCE =
-      PathPlanner.loadPathGroup("RedLongSide2.5Balance", new PathConstraints(4.0, 3.0));
-  public static final List<PathPlannerTrajectory> RED_SHORT_SIDE_2_CONE_BALANCE =
-      PathPlanner.loadPathGroup("RedShortSide2Balance", new PathConstraints(3.3, 3.3));
-  public static final List<PathPlannerTrajectory> RED_SHORT_SIDE_2_5_CONE_BALANCE =
-      PathPlanner.loadPathGroup("RedShortSide2.5Balance", new PathConstraints(2.0, 2.0));
+    return load(auto.pathName, auto.constraints);
+  }
+
+  private List<PathPlannerTrajectory> load(String pathName, PathConstraints constraints) {
+    if (!CACHING_ENABLED) {
+      return PathPlanner.loadPathGroup(pathName, constraints);
+    }
+
+    if (cache.containsKey(pathName)) {
+      WeakReference<List<PathPlannerTrajectory>> ref = cache.get(pathName);
+      if (ref.get() != null) {
+        return ref.get();
+      }
+    }
+
+    List<PathPlannerTrajectory> path = PathPlanner.loadPathGroup(pathName, constraints);
+    cache.put(pathName, new WeakReference<>(path));
+    return path;
+  }
 }
