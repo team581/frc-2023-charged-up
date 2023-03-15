fc217c842dc0bfc89b6e8f7312a77e280f877c5e
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2023-03-14 18:00:00 -0700

    Start clampy claw v2

--- src/main/java/frc/robot/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/intake/IntakeSubsystem.java
@@ -5,11 +5,7 @@
 package frc.robot.intake;
 
 import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
-import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import edu.wpi.first.math.filter.Debouncer;
-import edu.wpi.first.math.filter.Debouncer.DebounceType;
-import edu.wpi.first.math.filter.LinearFilter;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.config.Config;
@@ -28,18 +24,6 @@ public class IntakeSubsystem extends LifecycleSubsystem {
 
   private final TalonFX motor;
 
-  private final LinearFilter coneFilterIntakeCurrent =
-      LinearFilter.movingAverage(Config.IS_SPIKE ? 48 : 30); // Was 24 for spike
-  private final LinearFilter cubeFilterIntakeCurrent =
-      LinearFilter.movingAverage(Config.IS_SPIKE ? 20 : 10); // Was 10 for spike
-  private final LinearFilter coneFilterOuttakeCurrent =
-      LinearFilter.movingAverage(Config.IS_SPIKE ? 28 : 30); // Was 14 for spike
-  private final LinearFilter cubeFilterOuttakeCurrent =
-      LinearFilter.movingAverage(Config.IS_SPIKE ? 20 : 10); // Was 10 for spike
-
-  private final Debouncer coneFilterSensor = new Debouncer(5 * 0.02, DebounceType.kBoth);
-  private final Debouncer cubeFilterSensor = new Debouncer(5 * 0.02, DebounceType.kBoth);
-
   public IntakeSubsystem(TalonFX motor) {
     super(SubsystemPriority.INTAKE);
 
@@ -53,75 +37,17 @@ public class IntakeSubsystem extends LifecycleSubsystem {
     return motor.isFwdLimitSwitchClosed() == 1;
   }
 
-  private boolean sensorHasCone() {
-    return motor.isRevLimitSwitchClosed() == 1;
-  }
-
   @Override
   public void robotPeriodic() {
     Logger.getInstance().recordOutput("Intake/Mode", mode.toString());
     Logger.getInstance().recordOutput("Intake/HeldGamePiece", gamePiece.toString());
     Logger.getInstance().recordOutput("Intake/Current", motor.getStatorCurrent());
     Logger.getInstance().recordOutput("Intake/Voltage", motor.getMotorOutputVoltage());
-    Logger.getInstance().recordOutput("Intake/ConeIntakeSensor", sensorHasCone());
     Logger.getInstance().recordOutput("Intake/CubeIntakeSensor", sensorHasCube());
   }
 
   @Override
-  public void enabledPeriodic() {
-    double coneIntakeCurrent = coneFilterIntakeCurrent.calculate(motor.getStatorCurrent());
-    double cubeIntakeCurrent = cubeFilterIntakeCurrent.calculate(motor.getStatorCurrent());
-    double coneOuttakeCurrent = coneFilterOuttakeCurrent.calculate(motor.getStatorCurrent());
-    double cubeOuttakeCurrent = cubeFilterOuttakeCurrent.calculate(motor.getStatorCurrent());
-    Logger.getInstance().recordOutput("Intake/FilteredConeIntakeCurrent", coneIntakeCurrent);
-    Logger.getInstance().recordOutput("Intake/FilteredCubeIntakeCurrent", cubeIntakeCurrent);
-    Logger.getInstance().recordOutput("Intake/FilteredConeOuttakeCurrent", coneOuttakeCurrent);
-    Logger.getInstance().recordOutput("Intake/FilteredCubeOuttakeCurrent", cubeOuttakeCurrent);
-
-    boolean coneSensor = coneFilterSensor.calculate(sensorHasCone());
-    boolean cubeSensor = cubeFilterSensor.calculate(sensorHasCube());
-    Logger.getInstance().recordOutput("Intake/FilteredConeIntakeSensor", coneSensor);
-    Logger.getInstance().recordOutput("Intake/FilteredCubeIntakeSensor", cubeSensor);
-
-    if (mode == IntakeMode.INTAKE_CUBE) {
-      if (cubeIntakeCurrent > 35 || cubeSensor) {
-        gamePiece = HeldGamePiece.CUBE;
-      }
-    } else if (mode == IntakeMode.INTAKE_CONE) {
-      if (coneIntakeCurrent > (Config.IS_SPIKE ? 45 : 70)
-          || coneSensor) { // TODO: Edit currents for tyke
-        gamePiece = HeldGamePiece.CONE;
-      }
-    } else if (mode == IntakeMode.OUTTAKE_CUBE) {
-      if (cubeOuttakeCurrent < (Config.IS_SPIKE ? 10 : 10) || !cubeSensor) {
-        gamePiece = HeldGamePiece.NOTHING;
-      }
-    } else if (mode == IntakeMode.OUTTAKE_CONE) {
-      if (coneOuttakeCurrent < 10 || !coneSensor) {
-        gamePiece = HeldGamePiece.NOTHING;
-      }
-    }
-
-    if (mode == IntakeMode.OUTTAKE_CUBE) {
-      motor.set(TalonFXControlMode.PercentOutput, -0.3);
-    } else if (mode == IntakeMode.OUTTAKE_CONE) {
-      motor.set(TalonFXControlMode.PercentOutput, 0.4);
-    } else if (gamePiece == HeldGamePiece.CUBE) {
-      motor.set(TalonFXControlMode.PercentOutput, 0.075);
-    } else if (gamePiece == HeldGamePiece.CONE) {
-      motor.set(TalonFXControlMode.PercentOutput, -0.1);
-    } else if (mode == IntakeMode.INTAKE_CUBE) {
-      motor.set(TalonFXControlMode.PercentOutput, 0.5);
-    } else if (mode == IntakeMode.INTAKE_CONE) {
-      motor.set(TalonFXControlMode.PercentOutput, -0.5);
-    } else if (mode == IntakeMode.MANUAL_INTAKE) {
-      motor.set(TalonFXControlMode.PercentOutput, 0.5);
-    } else if (mode == IntakeMode.MANUAL_OUTTAKE) {
-      motor.set(TalonFXControlMode.PercentOutput, -0.5);
-    } else {
-      motor.set(TalonFXControlMode.PercentOutput, 0);
-    }
-  }
+  public void enabledPeriodic() {}
 
   public void setMode(IntakeMode mode) {
     if (this.mode != mode && (mode == IntakeMode.INTAKE_CONE || mode == IntakeMode.INTAKE_CUBE)) {
