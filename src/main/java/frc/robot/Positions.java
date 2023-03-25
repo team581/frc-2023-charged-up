8008bd95fe997e8b7c52a36e88b099381590a7cb
Jordan <117786700+jordanjcoderman@users.noreply.github.com>
2023-03-25 16:04:05 -0700

    Adjust floor cone intake position

--- src/main/java/frc/robot/Positions.java
+++ src/main/java/frc/robot/Positions.java
@@ -43,7 +43,7 @@ public class Positions {
 
   public static final SuperstructurePosition INTAKING_CONE_FLOOR =
       Config.IS_SPIKE
-          ? new SuperstructurePosition(2, Rotation2d.fromDegrees(141), -1)
+          ? new SuperstructurePosition(2.75, Rotation2d.fromDegrees(143), -1)
           : new SuperstructurePosition(3.39, Rotation2d.fromDegrees(161), -1);
   public static final SuperstructurePosition INTAKING_CONE_SHELF =
       Config.IS_SPIKE
